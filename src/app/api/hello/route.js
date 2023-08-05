733b4c32259c42ac7c294bb4341f7d0605662ffb
ishikiriyama-addix <ishikiriyama@addix.co.jp>
2023-08-05 23:37:47 +0900

    ファイルアップロード

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,3 +1,18 @@
-export async function GET(request) {
-  return new Response('Hello, Next.js!')
+import { NextRequest } from "next/server"
+
+// export async function GET(request) {
+//   const { searchParams } = new URL(request.url)
+//   console.log("GET request", searchParams.get("name"))
+
+//   return new Response(JSON.stringify({ message: "Hello World" }),{
+//     status: 401,
+//   })
+// }
+
+export async function POST(request) {
+
+  const body =await request.json()
+  const returnBody =`POSTで受け取った値：${body.name}`
+
+  return new Response(JSON.stringify({ body:returnBody}))
 }
